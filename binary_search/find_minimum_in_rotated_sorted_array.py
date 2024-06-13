ff0d70f73d2dc0c4bc2c486827a3be8ef1b82a49
Bahromjon <hopebegmuhammadov@gmail.com>
2024-06-13 22:57:32 +0500

    "find minimum in rotated sorted array" is solved

---
+++ binary_search/find_minimum_in_rotated_sorted_array.py
@@ -0,0 +1,43 @@
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        start = 0
+        end = len(nums) - 1
+        curr_min = float('inf')
+        while start < end:
+            mid = start + (end - start) // 2
+            if nums[mid] < curr_min:
+                curr_min = nums[mid]
+
+            if nums[start] < nums[mid]:
+                # search right side
+                start = mid + 1
+            else:
+                # search left side
+                end = mid - 1
+
+        return curr_min
+
+
+
+    def findMinBrute(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        start = 0
+        end = len(nums) - 1
+        while start < end:
+            if end - start == 1 or nums[start] < nums[end]:
+                return min(nums[start], nums[end])
+            if nums[start] > nums[end]:
+                mid = start + (end - start) // 2
+                if nums[start] > nums[mid]:
+                    end = mid
+                else:
+                    start = mid
+
+
+nums = [4, 5, 1, 2]
+s = Solution()
+print(s.findMin(nums))
