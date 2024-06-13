513d59e1de10bb83f91d2c7d51128032dfe5c7c6
Bahromjon <hopebegmuhammadov@gmail.com>
2024-06-13 18:30:38 +0500

    "valid parentheses" is solved

---
+++ stack/valid_parentheses.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {"}": "{", "]": "[", ")": "("}
+        for p in s:
+            if p not in pairs:
+                stack.append(p)
+            elif stack and stack[-1] == pairs[p]:
+                stack.pop()
+            else:
+                return False
+        return not stack
