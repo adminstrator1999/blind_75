29c522ee883aafd4b79271d9c4ed5125a9e656ac
Bahromjon <hopebegmuhammadov@gmail.com>
2024-06-22 22:58:06 +0500

    "same tree" is solved

---
+++ trees/same_tree.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+from trees.invert_binary_tree import TreeNode
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return False
