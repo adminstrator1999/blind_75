ea54f0c18a2912cecc3c91d60a5088776a11a3c2
Bahromjon <hopebegmuhammadov@gmail.com>
2024-06-24 12:40:19 +0500

    "subtree of another tree" is solved

---
+++ trees/subtree_of_another_tree.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from trees.invert_binary_tree import TreeNode
+
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot:
+            return False
+        if not root:
+            return False
+
+        if self.isSame(root, subRoot):
+            return True
+
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def isSame(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root and not subRoot:
+            return True
+        if root and subRoot and root.val == subRoot.val:
+            return self.isSame(root.left, subRoot.left) and self.isSame(root.right, subRoot.right)
+        return False
+
+
