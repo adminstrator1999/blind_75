65e225291f304a501f5187077a5055cd406b26da
Bahromjon <hopebegmuhammadov@gmail.com>
2024-08-17 19:12:20 +0500

    "Sum of Two Integers" is solved

--- bit_manipulation/sum_of_two_integers.py
+++ bit_manipulation/sum_of_two_integers.py
@@ -3,7 +3,23 @@ from typing import List
 
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        return a + b
+        def add(a, b):
+            if not a or not b:
+                return a or b
+            return add(a ^ b, (a & b) << 1)
+
+        if a * b < 0:
+            if a > 0:
+                return self.getSum(b, a)
+            if add(~a, 1) == b:
+                return 0
+            if add(~a, 1) < b:
+                return add(~add(add(~a, 1), add(~b, 1)), 1)
+
+        return add(a, b)
+
+    # def getSum(self, a: int, b: int) -> int:
+    #     return a + b
 
 
 a = 1
