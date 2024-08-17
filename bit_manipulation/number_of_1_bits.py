83b1e78d58e37e5f85e13013d4210dc3b885af62
Bahromjon <hopebegmuhammadov@gmail.com>
2024-08-17 17:28:04 +0500

    "Number of 1 Bits" is solved

---
+++ bit_manipulation/number_of_1_bits.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution
+
+    def hammingWeight(self, n: int) -> int:
+        s=format(n,"b")
+        s1=str(s)
+        return s1.count("1")
+
+    # def hammingWeight(self, n: int) -> int:
+    #     res = 0
+    #     while n:
+    #         n &= n - 1
+    #         res += 1
+    #     return res
+
+    # def hammingWeight(self, n: int) -> int:
+    #     count = 0
+    #     while n > 1:
+    #         if n % 2 == 1:
+    #             count += 1
+    #         n = n // 2
+    #     return count + 1
+
+n = 2147483645
+solution = Solution()
+print(solution.hammingWeight(n))
