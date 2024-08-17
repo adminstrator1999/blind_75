8e9a194c15634157bf6ab2bd78a394df3de60a0e
Bahromjon <hopebegmuhammadov@gmail.com>
2024-08-17 19:08:24 +0500

    "Sum of Two Integers" is solved

---
+++ bit_manipulation/sum_of_two_integers.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        return a + b
+
+
+a = 1
+b = 2
+solution = Solution()
+print(solution.getSum(a, b))
